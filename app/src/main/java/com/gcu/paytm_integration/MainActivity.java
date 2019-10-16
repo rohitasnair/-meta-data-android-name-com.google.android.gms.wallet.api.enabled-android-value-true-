c00e659ff93ec12d1999056b902c06210618425c
Rohit A S Nair <rohitasnair111@gmail.com>
2019-10-16 20:21:43 +0530

    first commit

---
+++ app/src/main/java/com/gcu/paytm_integration/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gcu.paytm_integration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
