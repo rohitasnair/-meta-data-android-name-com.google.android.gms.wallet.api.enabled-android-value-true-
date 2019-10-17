9c4d32be5767f424b98d2b2147f3b596d84e373d
Rohit A S Nair <rohitasnair111@gmail.com>
2019-10-18 00:28:52 +0530

    initial test ok

--- app/src/main/java/com/gcu/paytm_integration/Constants.java
+++ app/src/main/java/com/gcu/paytm_integration/Constants.java
@@ -1,10 +1,10 @@
 package com.gcu.paytm_integration;
 
 public class Constants {
-    public static final String M_ID = "dPNTkZ89809028362139"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String M_ID = "RFYmds71503775183836"; //Paytm Merchand Id we got it in paytm credentials
     public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
     public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
 
-    public static final String WEBSITE = "WEBSTAGING";
+    public static final String WEBSITE = "DEFAULT";
     public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";  //https://securegw.paytm.in/theia/paytmCallback
 }
